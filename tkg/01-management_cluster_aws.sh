e14f217bf791523c5321907397bf6a483b252b85
Benoit Moussaud <benoit@moussaud.org>
2021-04-02 15:48:50 +0200

    tkg cluster

--- tkg/01-management_cluster_aws.sh
+++ tkg/01-management_cluster_aws.sh
@@ -1,7 +1,7 @@
 # Ref: http://theblasfrompas.blogspot.com/2020/05/creating-my-first-tanzu-kubernetes-grid.html
 # Ref: https://stackoverflow.com/questions/415677/how-to-replace-placeholders-in-a-text-file
 
-# tested TKG 13
+# tested TKG 13/AWS
 # dependencies : bash, tanzu , envsubst, aws cli
 # CloudGate -> PowerUser
 export AWS_DEFAULT_REGION=eu-west-3
---
+++ tkg/02-new_cluster_aws.sh
@@ -0,0 +1,29 @@
+# Ref: http://theblasfrompas.blogspot.com/2020/05/creating-my-first-tanzu-kubernetes-grid.html
+# Ref: https://stackoverflow.com/questions/415677/how-to-replace-placeholders-in-a-text-file
+
+# tested TKG 13/AWS
+# dependencies : bash, tanzu , envsubst, 
+# CloudGate -> PowerUser
+export AWS_DEFAULT_REGION=eu-west-3
+export CLUSTER_NAME="aws-one"
+
+if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
+    echo "please provide AWS_ACCESS_KEY_ID"
+    exit 1
+fi
+
+if [[ -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
+    echo "please provide AWS_SECRET_ACCESS_KEY"
+    exit 1
+fi
+
+if [[ -z "${AWS_SESSION_TOKEN}" ]]; then
+    echo "please provide AWS_SESSION_TOKEN"
+    exit 1
+fi
+
+envsubst < ./aws-cluster-template.yaml  | grep -v "#" > aws-cluster.yaml
+
+echo "create the cluster ${CLUSTER_NAME}"
+tanzu cluster create --file aws-cluster.yaml --verbose 6 
+
