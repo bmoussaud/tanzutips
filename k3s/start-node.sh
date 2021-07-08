834fb70476bd7f477bc2e886919dac28aa8572d3
Benoit Moussaud <benoit@moussaud.org>
2021-07-08 15:00:22 +0200

    fix script

--- k3s/start-node.sh
+++ k3s/start-node.sh
@@ -1,4 +1,5 @@
-export K3S_TOKEN="K10ba7b6f17d0015bf45044e0602e068dc31658ecdabe942f50a26390edd4f935eb::server:712915e056949934faecc502b3e88f75"
-export K3S_URL="https://52.47.196.27:6443"
+export K3S_TOKEN="benoitK10ba7b6f17d0015bf45044e0602e068dc31658ecdabe942f50a26390edd4f935eb::server:712915e056949934faecc502b3e88f75"
+export K3S_URL="https://52.47.196.27:6443benoi"
+
 #sudo k3s agent --server https://${MASTER}:6443 --token ${NODE_TOKEN}
 curl -sfL https://get.k3s.io | sh -
