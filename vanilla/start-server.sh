70058661226037050c74a4611b649f8fd15c24a2
Benoit Moussaud <benoit@moussaud.org>
2021-07-12 17:48:56 +0200

    manage kubectl

--- vanilla/start-server.sh
+++ vanilla/start-server.sh
@@ -1,4 +1,5 @@
-sudo apt-get update -y
+#sudo apt-get update -y
+
 export K3S_KUBECONFIG_MODE="0644"
 curl -sfL https://get.k3s.io | sh -
 sudo service k3s status
@@ -8,6 +9,9 @@ mkdir ~/.kube/
 sudo cp  /etc/rancher/k3s/k3s.yaml ~/.kube/config
 sudo chmod 666 ~/.kube/config
 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
 echo "K3S_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)"
 
 
