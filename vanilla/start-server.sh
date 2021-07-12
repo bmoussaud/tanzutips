ed2ae8702252aa0240e3a2201b01c033cd207133
Benoit Moussaud <benoit@moussaud.org>
2021-07-12 16:20:30 +0200

    start-server.sh

--- vanilla/start-server.sh
+++ vanilla/start-server.sh
@@ -1,6 +1,9 @@
 sudo apt-get update -y
 curl -sfL https://get.k3s.io | sh -
+
+sudo k3s server &
 # Kubeconfig is written to /etc/rancher/k3s/k3s.yaml
 mkdir ~/.kube/
 sudo cp  /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chmod 666 ~/.kube/config
 
