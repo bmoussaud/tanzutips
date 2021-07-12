9cfcf34432d4b7274050f63d7e45352d05958301
Benoit Moussaud <benoit@moussaud.org>
2021-07-12 16:18:40 +0200

    kubeconfig server

--- vanilla/start-server.sh
+++ vanilla/start-server.sh
@@ -1,3 +1,6 @@
 sudo apt-get update -y
 curl -sfL https://get.k3s.io | sh -
+# Kubeconfig is written to /etc/rancher/k3s/k3s.yaml
+mkdir ~/.kube/
+sudo cp  /etc/rancher/k3s/k3s.yaml ~/.kube/config
 
