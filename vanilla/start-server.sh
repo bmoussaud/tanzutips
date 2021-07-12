bbc3fb95472848a7ca90b4f11eb4c5e63b03d4e3
Benoit Moussaud <benoit@moussaud.org>
2021-07-12 16:38:38 +0200

    start server vanilla

--- vanilla/start-server.sh
+++ vanilla/start-server.sh
@@ -1,9 +1,13 @@
 sudo apt-get update -y
+export K3S_KUBECONFIG_MODE="0644"
 curl -sfL https://get.k3s.io | sh -
+sudo service k3s status
 
-sudo k3s server --write-kubeconfig-mode
 # Kubeconfig is written to /etc/rancher/k3s/k3s.yaml
 mkdir ~/.kube/
 sudo cp  /etc/rancher/k3s/k3s.yaml ~/.kube/config
 sudo chmod 666 ~/.kube/config
+
+echo "K3S_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)"
+
 
