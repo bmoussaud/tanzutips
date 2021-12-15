c6720f6f85e8cfbf8fcf27a5f732457a553ec1f4
Benoit Moussaud <benoit@moussaud.org>
2021-12-15 14:56:51 +0100

    azure-management-cluster-template

--- tkg/01-management_cluster_azure.sh
+++ tkg/01-management_cluster_azure.sh
@@ -1,11 +1,12 @@
 # Ref: http://theblasfrompas.blogspot.com/2020/05/creating-my-first-tanzu-kubernetes-grid.html
 # Ref: https://stackoverflow.com/questions/415677/how-to-replace-placeholders-in-a-text-file
 
-# tested TKG 13/AZURE
+# tested TKG 14/AZURE
 # dependencies : bash, tanzu , envsubst, aws cli
 
 export AZURE_LOCATION=francecentral
-export CLUSTER_NAME=${CLUSTER_NAME:-bmoussaud-mgt-azure-cluster}
+export CLUSTER_NAME=${CLUSTER_NAME:-bmoussaud-az-paris-mgt-cluster}
+export CLUSTER_RG=tkg-paris
 
 if [[ -z "${AZURE_SUBSCRIPTION_ID}" ]]; then
     echo "please provide AZURE_SUBSCRIPTION_ID"
@@ -29,7 +30,10 @@ fi
 
 export AZURE_SSH_PUBLIC_KEY_B64=$(cat ~/.ssh/id_rsa.pub | awk {'print $1,$2'} | base64)
 
-envsubst < ./azure-management-cluster-template.yaml | grep -v '""' | grep -v "#" > azure-management-cluster.yaml
+envsubst < ./azure-management-cluster-template.yaml | grep -v '""' | grep -v "#" | sort > azure-management-cluster.yaml
+
+
+cat azure-management-cluster.yaml
 
 echo "create the management cluster"
 tanzu management-cluster create --file azure-management-cluster.yaml -v 6 
