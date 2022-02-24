a40b58cc8259e1b71497b8e74168bd6e26e9673d
Benoit Moussaud <benoit@moussaud.org>
2022-02-24 15:31:21 +0100

    eks + minor update

--- smoketests/01-whoami.sh
+++ smoketests/01-whoami.sh
@@ -1,5 +1,5 @@
 kubectl create ns dev
-kubectl create deployment whoami --image=harbor.mytanzu.xyz/library/whoami@sha256:e6d0a6d995c167bd339fa8b9bb2f585acd9a6e505a6b3fb6afb5fcbd52bbefb8 -n dev
+kubectl create deployment whoami --image=containous/whoami -n dev
 kubectl expose deployment/whoami --port=80 --target-port=80 -n dev
 kubectl get all -n dev
 kubectl -n dev get events
