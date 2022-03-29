b487ea20212e578d619c5ea5a8df4385479d87e5
Benoit Moussaud <benoit@moussaud.org>
2022-03-29 16:00:21 +0200

    whoami.sh with NS env var

--- smoketests/01-whoami.sh
+++ smoketests/01-whoami.sh
@@ -1,6 +1,7 @@
-kubectl create ns dev
-kubectl create deployment whoami --image=containous/whoami -n dev
-kubectl expose deployment/whoami --port=80 --target-port=80 -n dev
-kubectl get all -n dev
-kubectl -n dev get events
-kubectl port-forward  service/whoami 1080:80 -n dev
+export NS=${NS:-dev}
+kubectl create ns ${NS}
+kubectl create deployment whoami --image=containous/whoami -n ${NS}
+kubectl expose deployment/whoami --port=80 --target-port=80 -n ${NS}
+kubectl get all -n ${NS}
+kubectl -n ${NS} get events
+kubectl port-forward  service/whoami 1080:80 -n ${NS}
