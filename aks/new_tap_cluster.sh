1ce5d9a1edd9c18e07478aa2a36fde2ef9125202
Benoit Moussaud <benoit@moussaud.org>
2022-08-23 16:32:48 +0200

    reduce the size of the aks cluster

--- aks/new_tap_cluster.sh
+++ aks/new_tap_cluster.sh
@@ -3,6 +3,7 @@ set -x
 export CLUSTER_NAME=${CLUSTER_NAME:-tap-demo}
 #az account list-locations
 export LOCATION=${LOCATION:-germanywestcentral}
+export NODES=${NODES:-3}
 
 az login
 az group create --location ${LOCATION} --name ${CLUSTER_NAME}
@@ -15,7 +16,10 @@ az feature register --name PodSecurityPolicyPreview --namespace Microsoft.Contai
 az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/PodSecurityPolicyPreview')].{Name:name,State:properties.state}"
 az provider register --namespace Microsoft.ContainerService
 
-az aks create --resource-group ${CLUSTER_NAME} --name ${CLUSTER_NAME} --node-count 4 --enable-addons monitoring --node-vm-size Standard_DS3_v2 --node-osdisk-size 500 --enable-pod-security-policy
+# az aks create --resource-group ${CLUSTER_NAME} --name ${CLUSTER_NAME} --node-count 4 --enable-addons monitoring --node-vm-size Standard_DS3_v2 --node-osdisk-size 500 --enable-pod-security-policy
+
+az aks create --resource-group ${CLUSTER_NAME} --name ${CLUSTER_NAME} --node-count ${NODES} --enable-addons monitoring --node-vm-size standard_b4ms  --enable-pod-security-policy
+
 
 #az aks get-credentials --resource-group ${CLUSTER_NAME} --name ${CLUSTER_NAME} --file ~/.kube/config-files/kubeconfig-${CLUSTER_NAME}.yml
 #az aks get-credentials --admin --resource-group ${CLUSTER_NAME} --name ${CLUSTER_NAME} --file ~/.kube/config-files/kubeconfig-admin-${CLUSTER_NAME}.yml
