d612d02574897926ef78b3dea2b3fa98c0f746ce
Benoit Moussaud <benoit@moussaud.org>
2023-01-17 14:42:56 +0100

    update smoke test

--- smoketests/01-whoami.sh
+++ smoketests/01-whoami.sh
@@ -1,7 +1,9 @@
 export NS=${NS:-dev}
 kubectl create ns ${NS}
-kubectl create deployment whoami --image=containous/whoami -n ${NS}
-kubectl expose deployment/whoami --port=80 --target-port=80 -n ${NS}
+kubectl create secret docker-registry regcred --docker-server=https://docker.io --docker-username=bmoussaud --docker-password=${DOCKER_PASSWORD} --docker-email=benoit@moussaud.org -n ${NS}
+#kubectl create deployment whoami --image=containous/whoami -n ${NS}
+kubectl apply -f deployment.yaml -n ${NS}
+kubectl expose deployment/whoami-deployment --port=80 --target-port=80 -n ${NS}
 kubectl get all -n ${NS}
 kubectl -n ${NS} get events
-kubectl port-forward  service/whoami 1080:80 -n ${NS}
+kubectl port-forward  service/whoami-deployment 1080:80 -n ${NS}
