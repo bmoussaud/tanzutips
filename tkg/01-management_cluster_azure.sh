5b104c74808ec0b65dba4bb6e0e8b6aa5c2724cc
Benoit Moussaud <benoit@moussaud.org>
2023-01-27 14:48:27 +0100

    update the tkg mc azure

--- tkg/01-management_cluster_azure.sh
+++ tkg/01-management_cluster_azure.sh
@@ -8,17 +8,23 @@ export AZURE_LOCATION=francecentral
 export CLUSTER_NAME=${CLUSTER_NAME:-bmoussaud-az-paris-mgt-cluster}
 export CLUSTER_RG=tkg-paris
 
-LOCAL_SSH_PUBLIC_KEY=~/.ssh/bmoussaud+azure_id_rsa.pub
+export LOCAL_SSH_PUBLIC_KEY=${LOCAL_SSH_PUBLIC_KEY:-/root/.ssh/1674826159_6842616.pub}
+export RESSOURCE_GROUP=${RESSOURCE_GROUP:-tkg21}
 
 if [[ -z "${AZURE_SUBSCRIPTION_ID}" ]]; then
     echo "please provide AZURE_SUBSCRIPTION_ID"
     exit 1
 fi
 
-if [[ -z "${AZURE_TENANT_ID}" ]]; then
-    echo "please provide AZURE_TENANT_ID"
-    exit 1
-fi
+echo "--- Create a service principal"
+AZURE_TENANT_ID=`az account list --query '[?isDefault].tenantId' -o tsv`
+echo "----  AZURE_TENANT_ID ${AZURE_TENANT_ID}"
+AZURE_ROLE=Owner
+AZURE_ROLE_NAME="BMOUSSAUD_TKG_${AZURE_ROLE}_for_${RESSOURCE_GROUP}"
+export AZURE_CLIENT_SECRET=`az ad sp create-for-rbac --name "${AZURE_ROLE_NAME}" --role $AZURE_ROLE --query 'password' -o tsv --scopes /subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${RESSOURCE_GROUP}
+echo "----  AZURE_CLIENT_SECRET ${AZURE_CLIENT_SECRET}"
+export AZURE_CLIENT_ID=`az ad sp list --display-name "${AZURE_ROLE_NAME}" --query '[0].appId' -o tsv`
+echo "----  AZURE_CLIENT_ID ${AZURE_CLIENT_ID}"
 
 if [[ -z "${AZURE_CLIENT_ID}" ]]; then
     echo "please provide AZURE_CLIENT_ID"
