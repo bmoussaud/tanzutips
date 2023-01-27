258769b741c691eb0f1c0dacb57585fe13aa5e7e
Benoit Moussaud <benoit@moussaud.org>
2023-01-27 14:50:58 +0100

    fix .shj

--- tkg/01-management_cluster_azure.sh
+++ tkg/01-management_cluster_azure.sh
@@ -21,7 +21,7 @@ AZURE_TENANT_ID=`az account list --query '[?isDefault].tenantId' -o tsv`
 echo "----  AZURE_TENANT_ID ${AZURE_TENANT_ID}"
 AZURE_ROLE=Owner
 AZURE_ROLE_NAME="BMOUSSAUD_TKG_${AZURE_ROLE}_for_${RESSOURCE_GROUP}"
-export AZURE_CLIENT_SECRET=`az ad sp create-for-rbac --name "${AZURE_ROLE_NAME}" --role $AZURE_ROLE --query 'password' -o tsv --scopes /subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${RESSOURCE_GROUP}
+export AZURE_CLIENT_SECRET=`az ad sp create-for-rbac --name "${AZURE_ROLE_NAME}" --role $AZURE_ROLE --query 'password' -o tsv --scopes /subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${RESSOURCE_GROUP}` 
 echo "----  AZURE_CLIENT_SECRET ${AZURE_CLIENT_SECRET}"
 export AZURE_CLIENT_ID=`az ad sp list --display-name "${AZURE_ROLE_NAME}" --query '[0].appId' -o tsv`
 echo "----  AZURE_CLIENT_ID ${AZURE_CLIENT_ID}"
